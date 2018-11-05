3928fae070446a5aac2b4347fe1eee2af9ad7fec
emmadal <emmanueldalougou@gmail.com>
2018-11-05 15:59:50 +0100

    decouvrir Aboisso completed

--- src/components/pages/DecouvrirVille.js
+++ src/components/pages/DecouvrirVille.js
@@ -1,9 +1,87 @@
 import React from 'react'
+import {description} from '../containers/Description'
+import '../styles/historiqueville.css'
 
 export default function DecouvrirVille() {
   return(
     <div>
-      <h1>Decouvrir ABOISSO</h1>
+      <div className="container">
+        <h1 className="mt-5 mb-5 text-green h1-responsive mytitle"> Decouvrir Aboisso </h1>
+        <div className="row">
+          <div className="col-md-12">
+            <img src={require('../img/ville_aboisso_large.png')} className="img-fluid rounded" alt="Histoire"/>
+          </div>          
+        </div>
+        <div className="row mt-4">
+          <div className="col-md-12">
+            <p className="text-justify descriptn">
+              {
+                description[2].text
+              }
+            </p>
+            <h4 className="h4-responsive mytitle mt-5">MILIEU NATUREL</h4>
+            <h5 className="h5-responsive mytitle">Relief</h5>
+            <p className="text-justify descriptn">
+              {
+              description[3].textrelief
+              }
+            </p>
+            <h5 className="h5-responsive mytitle">Hydrographie</h5>
+            <p className="text-justify descriptn">
+              {
+              description[4].textclimat
+              }
+            </p>
+            <ul>
+              <li className="descriptn">une saison sèche de Décembre à Février</li>
+              <li className="descriptn">une grande saison pluvieuse de Mars à Juillet</li>
+              <li className="descriptn">une petite saison sèche d'Août à Septembre</li>
+              <li className="descriptn">une petite saison pluvieuse d'Octobre à Novembre</li>
+            </ul>
+            <h5 className="h5-responsive mytitle">Vegetation</h5>
+            <p className="text-justify descriptn">
+              {
+              description[5].textvegetation
+              }
+            </p>
+            <ul>
+              <li className="descriptn">la forêt classée de Soumié de 725 ha</li>
+              <li className="descriptn">la forêt classée de Nègué de 350 ha</li>
+            </ul>
+            <h4 className="h4-responsive mytitle mt-5">MILIEU HUMAIN</h4>
+            <h5 className="h5-responsive mytitle">Population</h5>
+            <p className="text-justify descriptn">
+              {
+              description[6].textpopulation
+              }
+            </p>
+            <h5 className="h5-responsive mytitle">Activités économiques</h5>
+            <p className="text-justify descriptn">
+              {
+              description[7].texteco
+              }
+            </p>
+            <h5 className="h5-responsive mytitle">Les cultures de rentes</h5>
+            <p className="text-justify descriptn">
+              {
+              description[8].textrentes
+              }
+            </p>
+            <h5 className="h5-responsive mytitle">Les cultures vivrières</h5>
+            <p className="text-justify descriptn">
+              {
+              description[9].textvivriers
+              }
+            </p>
+            <h5 className="h5-responsive mytitle">Autres activités</h5>
+            <p className="text-justify descriptn">
+              {
+              description[10].textautres
+              }
+            </p>
+          </div>          
+        </div>
+      </div>
     </div>
   )
 }
