cd31a3f6453a742cf45270694595ad117f3ae838
emmadal <emmanueldalougou@gmail.com>
2018-11-10 18:09:13 +0100

    add logo

--- src/components/Banniere.js
+++ src/components/Banniere.js
@@ -7,16 +7,21 @@ export default function Banniere() {
     <div className="">
         <div className="container">
           <div className="row mybanniere">
-            <div className="col-md-4 ">
+            <div className="col-md-3">
               <img src={require('../components/img/BANNIERE.jpg')} alt="banniere"
                 className="mybanniere"
               />
             </div>
-            <div className="col-md-8">
+            <div className="col-md-7 text-center">
               <h3 className="h3-responsive textbaniere">MAIRE DE LA VILLE D'ABOISSO</h3>
               <h5 className="h5-responsive textbaniere">Le vrai changement avec Jeremie Alfred
               N'Gouan
               </h5>
+            </div>
+            <div className="col-md-2">
+              <img src={require('../components/img/logo.jpg')} alt="banniere"
+                className="mybanniere"
+              />
             </div>
           </div>
         </div>
