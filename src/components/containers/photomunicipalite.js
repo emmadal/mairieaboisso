994e71232c82177c672ca094c250a9c9c96f81da
emmadal <emmanueldalougou@gmail.com>
2019-04-19 14:55:00 +0200

    add picture adjoint maire

---
+++ src/components/containers/photomunicipalite.js
@@ -0,0 +1,42 @@
+const photomunicipalite = [
+  {
+    id: 1,
+    img: require("../img/1eradjoint.jpg"),
+    fonction: "1er Adjoint au Maire",
+    name: "BILE KASSI ALPHONSE"
+  },
+  {
+    id: 2,
+    img: require("../img/2eadjoint.jpg"),
+    fonction: "2e Adjoint au Maire",
+    name: "BENIE JEAN PAU"
+  },
+  {
+    id: 3,
+    img: require("../img/3eadjoint.jpg"),
+    fonction: "3e Adjoint au Maire",
+    name: "KOFFI EKUE EPSE KADJA"
+  },
+  {
+    id: 4,
+    img: require("../img/4eadjoint.jpg"),
+    fonction: "4e Adjoint au Maire",
+    name: "ADJAFFI ANGELINE"
+  },
+  {
+    id: 5,
+    img: require("../img/5eadjoint.jpg"),
+    fonction: "5e Adjoint au Maire",
+    name: "DOUMBIA DAOUDA"
+  }
+]
+const photomaire = [
+  {
+    id: 5,
+    img: require("../img/maire.jpg"),
+    fonction: "Maire",
+    name: "JEREMIE ALFRED N'GOUAN"
+  }
+]
+
+export {photomaire, photomunicipalite}
--- src/components/pages/Presentation.js
+++ src/components/pages/Presentation.js
@@ -1,21 +1,50 @@
-import React, {useState}from 'react'
-import { Container, TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
-import {Collapse} from 'mdbreact'
-import {mairie, cabinet, cabinetservices, secretariat, secretariatservices, direction, directionservices} from '../containers/mairie'
-import {maire} from '../containers/presentationmaire'
-import {conseilmunicipal,
-    attributions,
-/*     competences,
+import React, { useState } from "react"
+import {
+  Container,
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Row,
+  Col,
+  Card,
+  CardImg, 
+  CardBody,
+  CardText,
+  CardTitle
+} from "reactstrap"
+import { Collapse, MDBCard, MDBCol,
+MDBCardImage,
+MDBCardBody,
+MDBCardTitle,
+MDBCardText,
+MDBBtn } from "mdbreact"
+import {
+  mairie,
+  cabinet,
+  cabinetservices,
+  secretariat,
+  secretariatservices,
+  direction,
+  directionservices
+} from "../containers/mairie"
+import { maire } from "../containers/presentationmaire"
+import { photomunicipalite, photomaire } from "../containers/photomunicipalite"
+import {
+  conseilmunicipal,
+  attributions,
+  /*     competences,
     detailscompetences, */
-    municipalite,
-    membresmunicipalite} from '../containers/conseilmunicipal'
+  municipalite,
+  membresmunicipalite
+} from "../containers/conseilmunicipal"
 
-import classnames from 'classnames';
-import '../styles/presentation.css'
+import classnames from "classnames"
+import "../styles/presentation.css"
 
 const Presentation = () => {
-
-  const [activeTab, setActiveTab] = useState('1')
+  const [activeTab, setActiveTab] = useState("1")
   const [collapseattr, setCollapseAttr] = useState(false)
   // const [collapsecomp, setCollapseComp] = useState(false)
 
@@ -27,137 +56,156 @@ const Presentation = () => {
   function setMyCollapseAttr() {
     setCollapseAttr(!collapseattr)
   }
-/*   function setMyCollapseComp() {
+  /*   function setMyCollapseComp() {
     setCollapseComp(!collapsecomp)
   }
  */
 
-  return(
+  return (
     <div>
       <Container>
-        <h3 className="mt-5 mb-5 text-green h3-responsive mytitle">Presentation de la Mairie</h3>
+        <h3 className="mt-5 mb-5 text-green h3-responsive mytitle">
+          Presentation de la Mairie
+        </h3>
         <Row>
-        	<Col sm="12">
-        		{
-        			mairie.map((text, key) =>(
-        			<p className="text-justify descriptn" key={key}>{text.info}</p>
-        			))
-        		}
-        	</Col>
+          <Col sm="12">
+            {mairie.map((text, key) => (
+              <p className="text-justify descriptn" key={key}>
+                {text.info}
+              </p>
+            ))}
+          </Col>
         </Row>
 
         {/*Debut des Tabs*/}
-          <Row className="mt-3">
-            <Col sm="12">
-              <Nav tabs>
-                  <NavItem>
-                    <NavLink
-                      className={classnames({ active: activeTab === '1' })}
-                      onClick={() => { toggle('1'); }}>
-                      <p className="mytitle font-weight-bold">LE MAIRE</p>
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      className={classnames({ active: activeTab === '2' })}
-                      onClick={() => { toggle('2'); }}>
-                      <p className="mytitle font-weight-bold">BIOGRAPHIE</p>
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      className={classnames({ active: activeTab === '3' })}
-                      onClick={() => { toggle('3'); }}>
-                      <p className="mytitle font-weight-bold">LE CABINET DU MAIRE</p>
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      className={classnames({ active: activeTab === '4' })}
-                      onClick={() => { toggle('4'); }}>
-                      <p className="mytitle font-weight-bold">LES SERVICES DE LA MARIE</p>
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      className={classnames({ active: activeTab === '5' })}
-                      onClick={() => { toggle('5'); }}>
-                      <p className="mytitle font-weight-bold">LE CONSEIL MUNICIPAL</p>
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      className={classnames({ active: activeTab === '6' })}
-                      onClick={() => { toggle('6'); }}>
-                      <p className="mytitle font-weight-bold">LA MUNICIPALITE</p>
-                    </NavLink>
-                  </NavItem>
-              </Nav>
-            </Col>
+        <Row className="mt-3">
+          <Col sm="12">
+            <Nav tabs>
+              <NavItem>
+                <NavLink
+                  className={classnames({ active: activeTab === "1" })}
+                  onClick={() => {
+                    toggle("1")
+                  }}
+                >
+                  <p className="mytitle font-weight-bold">LE MAIRE</p>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className={classnames({ active: activeTab === "2" })}
+                  onClick={() => {
+                    toggle("2")
+                  }}
+                >
+                  <p className="mytitle font-weight-bold">BIOGRAPHIE</p>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className={classnames({ active: activeTab === "3" })}
+                  onClick={() => {
+                    toggle("3")
+                  }}
+                >
+                  <p className="mytitle font-weight-bold">
+                    LE CABINET DU MAIRE
+                  </p>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className={classnames({ active: activeTab === "4" })}
+                  onClick={() => {
+                    toggle("4")
+                  }}
+                >
+                  <p className="mytitle font-weight-bold">
+                    LES SERVICES DE LA MARIE
+                  </p>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className={classnames({ active: activeTab === "5" })}
+                  onClick={() => {
+                    toggle("5")
+                  }}
+                >
+                  <p className="mytitle font-weight-bold">
+                    LE CONSEIL MUNICIPAL
+                  </p>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className={classnames({ active: activeTab === "6" })}
+                  onClick={() => {
+                    toggle("6")
+                  }}
+                >
+                  <p className="mytitle font-weight-bold">LA MUNICIPALITE</p>
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Col>
 
-            {/*Contenu des Tabs*/}
+          {/*Contenu des Tabs*/}
 
-              <TabContent activeTab={activeTab}>
+          <TabContent activeTab={activeTab}>
+            <TabPane tabId="1">
+              {maire.map((text, key) => (
+                <p key={key} className="text-justify descriptn mt-3 mx-1 px-3">
+                  {text.info}
+                </p>
+              ))}
+            </TabPane>
+            <TabPane tabId="2" />
 
-                <TabPane tabId="1">
-                  {
-                    maire.map((text, key) => (
-                      <p key={key} className="text-justify descriptn mt-3 mx-1 px-3">{text.info}</p>
-                    ))
-                  }
-                </TabPane>
-                <TabPane tabId="2"></TabPane>
+            <TabPane tabId="3">
+              {cabinet.map((text, key) => (
+                <p key={key} className="text-justify descriptn mt-3 p-3">
+                  {text.info}
+                </p>
+              ))}
+              {cabinetservices.map((text, key) => (
+                <ul key={key}>
+                  <li className="text-justify descriptn px-2">{text.info}</li>
+                </ul>
+              ))}
+            </TabPane>
 
-                <TabPane tabId="3">
-                  {
-                    cabinet.map((text, key) => (
-                      <p key={key} className="text-justify descriptn mt-3 p-3">{text.info}</p>
-                    ))
-                  }
-                  {
-                    cabinetservices.map((text, key) => (
-                      <ul key={key} >
-                        <li className="text-justify descriptn px-2">{text.info}</li>
-                      </ul>
-                    ))
-                  }
-                </TabPane>
+            <TabPane tabId="4">
+              {secretariat.map((text, key) => (
+                <p key={key} className="text-justify descriptn mt-3 p-3">
+                  {text.info}
+                </p>
+              ))}
+              {secretariatservices.map((text, key) => (
+                <ul key={key}>
+                  <li className="text-justify descriptn px-2">{text.info}</li>
+                </ul>
+              ))}
+              {direction.map((text, key) => (
+                <p key={key} className="text-justify descriptn mt-3 p-3">
+                  {text.info}
+                </p>
+              ))}
+              {directionservices.map((text, key) => (
+                <ul key={key}>
+                  <li className="text-justify descriptn px-2">{text.info}</li>
+                </ul>
+              ))}
+            </TabPane>
 
-                <TabPane tabId="4">
-                  {
-                    secretariat.map((text, key) => (
-                      <p key={key} className="text-justify descriptn mt-3 p-3">{text.info}</p>
-                    ))
-                  }
-                  {
-                    secretariatservices.map((text, key) => (
-                      <ul key={key}>
-                        <li className="text-justify descriptn px-2">{text.info}</li>
-                      </ul>
-                    ))
-                  }
-                  {
-                    direction.map((text, key) => (
-                      <p key={key} className="text-justify descriptn mt-3 p-3">{text.info}</p>
-                    ))
-                  }
-                  {
-                    directionservices.map((text, key) => (
-                      <ul key={key}>
-                        <li className="text-justify descriptn px-2">{text.info}</li>
-                      </ul>
-                    ))
-                  }
-                </TabPane>
-
-                <TabPane tabId="5">
-                  {
-                    conseilmunicipal.map((text, key) => (
-                      <p key={key} className="text-justify descriptn mt-3 p-3">{text.info}</p>
-                    ))
-                  }
-                  <Row className=" p-3">
-                    {/* <Col sm="6">
+            <TabPane tabId="5">
+              {conseilmunicipal.map((text, key) => (
+                <p key={key} className="text-justify descriptn mt-3 p-3">
+                  {text.info}
+                </p>
+              ))}
+              <Row className=" p-3">
+                {/* <Col sm="6">
                       <NavLink>
                         <h5 className="text-justify text-center h5-responsive mytitle"
                           onClick={setMyCollapseComp}>
@@ -179,51 +227,70 @@ const Presentation = () => {
                         }
                       </Collapse>
                     </Col> */}
-                    <Col sm="12">
-                      <NavLink>
-                        <h5 className="text-justify h5-responsive text-center mytitle"
-                          onClick={setMyCollapseAttr}>
-                          Les attributions du Conseil Municipal
-                        </h5>
-                      </NavLink>
-                      <Collapse isOpen={collapseattr}>
-                        {
-                          attributions.map((text, key) =>(
-                            <p key={key} className="text-justify descriptn mt-3 p-3">{text.info}</p>
-                          ))
-                        }
-                      </Collapse>
-                    </Col>
-                  </Row>
-                </TabPane>
+                <Col sm="12">
+                  <NavLink>
+                    <h5
+                      className="text-justify h5-responsive text-center mytitle"
+                      onClick={setMyCollapseAttr}
+                    >
+                      Les attributions du Conseil Municipal
+                    </h5>
+                  </NavLink>
+                  <Collapse isOpen={collapseattr}>
+                    {attributions.map((text, key) => (
+                      <p key={key} className="text-justify descriptn mt-3 p-3">
+                        {text.info}
+                      </p>
+                    ))}
+                  </Collapse>
+                </Col>
+              </Row>
+            </TabPane>
 
-                <TabPane tabId="6">
-                  {
-                    municipalite.map((text, key) => (
-                      <p key={key} className="text-justify descriptn mt-3 mx-1 px-3">{text.info}</p>
-                    ))
-                  }
-                  <p className="text-justify descriptn mt-3 mx-1 px-3">
-                    Les membres de la Municipalité :
-                  </p>
-                  {
-                    membresmunicipalite.map((text, key) => (
-                      <ul key={key} >
-                        <li className="text-justify descriptn px-2 font-weight-bold">{text.info}</li>
-                      </ul>
-                    ))
-                  }
-                </TabPane>
+            <TabPane tabId="6">
+              {municipalite.map((text, key) => (
+                <p key={key} className="text-justify descriptn mt-3 mx-1 px-3">
+                  {text.info}
+                </p>
+              ))}
+              <p className="text-justify descriptn mt-3 mx-1 px-3">
+                Les membres de la Municipalité :
+              </p>
+              <Col sm="12" className="text-center rowmaire">
+                <Card>
+                  <CardImg top width="100%" classname="img-responsive" src={photomaire[0].img}/>
+                  <CardBody>
+                    <CardTitle className="text-center text-justify font-weight-bold descriptn">{photomaire[0].name}</CardTitle>
+                    <CardText className="text-center font-weight-bold descriptn">{photomaire[0].fonction}</CardText>
+                  </CardBody>
+                </Card>  
+              </Col>
+              <Container className="mt-4">
+                <Row >
+                  <MDBCol sm="12" className="rowadjoint">
+                    {
+                      photomunicipalite.map((i, w) => (
+                        <MDBCard style={{ width: "22rem" }} className="my-3">
+                          <MDBCardImage className="img-fluid rounded" src={i.img} waves />
+                          <MDBCardBody>
+                            <MDBCardTitle>{i.name}</MDBCardTitle>
+                            <MDBCardText> { i.fonction } </MDBCardText>
+                          </MDBCardBody>
+                        </MDBCard>                  
+                      ))
+                    }
+                  </MDBCol>
+                </Row>
+              </Container>
+            </TabPane>
+          </TabContent>
 
-              </TabContent>
-
-            {/*Contenu des Tabs*/}
-
-          </Row>
+          {/*Contenu des Tabs*/}
+        </Row>
         {/*Debut des Tabs*/}
       </Container>
     </div>
   )
 }
 
-export default Presentation;
+export default Presentation
