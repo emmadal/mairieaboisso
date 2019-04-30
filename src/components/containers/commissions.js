11e2e80375753548a2f24eb5b2986da5ef82c69b
emmadal <emmanueldalougou@gmail.com>
2019-04-30 10:27:46 +0200

    commissions effectuee

---
+++ src/components/containers/commissions.js
@@ -0,0 +1,161 @@
+export const commissions = [
+  {
+    id: 1,
+    info: `Le rôle des commissions municipales est d'étudier les questions soumises au Conseil, sur lesquelles elles émettent un avis. Elles sont également un lieu de propositions et d'échanges. 
+    
+    Chaque commission est présidée par un adjoint au Maire. Elles se réunissent généralement une fois par mois.Lors du conseil municipal du 25 avril 2019, les élus ont voté la constitution de 4commissions municipales en charge d'étudier les questions qui leur sont soumises.
+    `
+  },
+  {
+    id: 2,
+    info: `Le maire, Jeremie N’gouan Alfred est le Président de Droit de l'ensemble de ces Commissions.`
+  }
+];
+
+export const comEco = [
+  {
+    id: 1,
+    name: `N’Gouan Jeremie Alfred`
+  },
+  {
+    id: 2,
+    name: `Agbia N’Gaman Lucien`
+  },
+  {
+    id: 3,
+    name: `Bile Khassy Alphonse`
+  },
+  {
+    id: 4,
+    name: `Benié Kakou Jean Paul`
+  },
+  {
+    id: 5,
+    name: `Adjaffi Epse Nanga Angeline`
+  },
+  {
+    id: 6,
+    name: `Yangba Dioro Emma Jacqueline Gomon`
+  },
+  {
+    id: 7,
+    name: `Borba Kotoua`
+  },
+  {
+    id: 8,
+    name: `Diakité Fédé`
+  },
+  {
+    id: 9,
+    name: `Ekponon Jean Claude`
+  }
+];
+
+export const comaffaires = [
+  {
+    id: 1,
+    name: `N’Gouan Jeremie Alfred`
+  },
+  {
+    id: 2,
+    name: `Amangoua Atcho`
+  },
+  {
+    id: 3,
+    name: `Doumbia Daouda`
+  },
+  {
+    id: 4,
+    name: `Aka Edjolohi Etienne`
+  },
+  {
+    id: 5,
+    name: `Koffi Ano N’Guessan`
+  },
+  {
+    id: 6,
+    name: `Malan Agniman Adjoba`
+  },
+  {
+    id: 7,
+    name: `Abdoulaye Traoré`
+  },
+  {
+    id: 8,
+    name: `Gueye Amadou Boubakar`
+  },
+  {
+    id: 9,
+    name: `N’Guessan Kouamé`
+  }
+];
+
+export const comsante = [
+  {
+    id: 1,
+    name: `N’Gouan Jeremie Alfred`
+  },
+  {
+    id: 2,
+    name: `Kassi Damoh Julien Sylvain`
+  },
+  {
+    id: 3,
+    name: `Soro Kolotchelema`
+  },
+  {
+    id: 4,
+    name: `Zoumana Sangaré`
+  },
+  {
+    id: 5,
+    name: `Mamadou Kano`
+  },
+  {
+    id: 6,
+    name: `Koné Abiba Epse Coulibaly`
+  },
+  {
+    id: 7,
+    name: `Ouattara Makafin Dite Mariame`
+  },
+  {
+    id: 8,
+    name: `Koman Obba Christine`
+  }
+];
+
+export const comfoncier = [
+  {
+    id: 1,
+    name: `N’Gouan Jeremie Alfred`
+  },
+  {
+    id: 2,
+    name: `Cissé Aboubakari`
+  },
+  {
+    id: 3,
+    name: `Aka Tchékou`
+  },
+  {
+    id: 4,
+    name: `Koffi Ekue Emilienne`
+  },
+  {
+    id: 5,
+    name: `Couloud Jean Yves Hervé`
+  },
+  {
+    id: 6,
+    name: `Bossombra Kassi`
+  },
+  {
+    id: 7,
+    name: `Sanhou Djedou`
+  },
+  {
+    id: 8,
+    name: `Brou Edgar Vincent`
+  }
+];
--- src/components/pages/Commissions.js
+++ src/components/pages/Commissions.js
@@ -1,5 +1,14 @@
-import React from "react"
-import { Container } from "mdbreact"
+import React from "react";
+import { Container, Col, Row } from "mdbreact";
+import {
+  commissions,
+  comEco,
+  comaffaires,
+  comsante,
+  comfoncier
+} from "../containers/commissions";
+import { Table } from "reactstrap";
+
 export default function Commissions() {
   return (
     <div>
@@ -9,6 +18,115 @@ export default function Commissions() {
           Les Commissions
         </h3>
       </Container>
+      <Container>
+        <Row>
+          <Col sm="12">
+            {commissions.map((i, key) => (
+              <p key={key} className="text-justify descriptn">
+                {i.info}
+              </p>
+            ))}
+          </Col>
+        </Row>
+        <Row>
+          <Col sm="3">
+            <details>
+              <summary className="h6-responsive text-center mytitle my-3">
+                commission des Affaires Economiques et Financières
+              </summary>
+              <Table bordered hover responsive className="my-3">
+                <thead>
+                  <tr>
+                    <th className="text-center">#</th>
+                    <th className="text-center">Nom et Prenoms</th>
+                  </tr>
+                </thead>
+                {comEco.map((i, key) => (
+                  <tbody key={key}>
+                    <tr>
+                      <th scope="row"> {i.id} </th>
+                      <th> {i.name} </th>
+                    </tr>
+                  </tbody>
+                ))}
+              </Table>
+            </details>
+          </Col>
+
+          <Col sm="3">
+            <details>
+              <summary className="h6-responsive text-center mytitle my-3">
+                commission des Affaires Sociales, Culturelles et la Promotion
+                Humaine
+              </summary>
+              <Table bordered hover responsive className="my-3">
+                <thead>
+                  <tr>
+                    <th className="text-center">#</th>
+                    <th className="text-center">Nom et Prenoms</th>
+                  </tr>
+                </thead>
+                {comaffaires.map((i, key) => (
+                  <tbody key={key}>
+                    <tr>
+                      <th scope="row"> {i.id} </th>
+                      <th> {i.name} </th>
+                    </tr>
+                  </tbody>
+                ))}
+              </Table>
+            </details>
+          </Col>
+
+          <Col sm="3">
+            <details>
+              <summary className="h6-responsive text-center mytitle my-3">
+                Commission Sante et Cadre de Vie
+              </summary>
+              <Table bordered hover responsive className="my-3">
+                <thead>
+                  <tr>
+                    <th className="text-center">#</th>
+                    <th className="text-center">Nom et Prenoms</th>
+                  </tr>
+                </thead>
+                {comsante.map((i, key) => (
+                  <tbody key={key}>
+                    <tr>
+                      <th scope="row"> {i.id} </th>
+                      <th> {i.name} </th>
+                    </tr>
+                  </tbody>
+                ))}
+              </Table>
+            </details>
+          </Col>
+
+          <Col sm="3">
+            <details>
+              <summary className="h6-responsive text-center mytitle my-3">
+                Commission Foncier
+              </summary>
+              <Table bordered hover responsive className="my-3">
+                <thead>
+                  <tr>
+                    <th className="text-center">#</th>
+                    <th className="text-center">Nom et Prenoms</th>
+                  </tr>
+                </thead>
+                {comfoncier.map((i, key) => (
+                  <tbody key={key}>
+                    <tr>
+                      <th scope="row"> {i.id} </th>
+                      <th> {i.name} </th>
+                    </tr>
+                  </tbody>
+                ))}
+              </Table>
+            </details>
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }
