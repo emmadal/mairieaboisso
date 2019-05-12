1fec8c3d814d0e9b7b996658e96e679e5d629165
emmadal <emmanueldalougou@gmail.com>
2019-05-12 12:40:24 +0200

    change banniere

--- src/components/Banniere.js
+++ src/components/Banniere.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles/banniere.css";
 import Menu from "./Menu";
+import ModalImage from "react-modal-image";
 
 export default function Banniere() {
   return (
@@ -8,15 +9,16 @@ export default function Banniere() {
       <div className="container">
         <div className="row mybanniere">
           <div className="col-md-3 text-center">
-            <img
-              src={require("../components/img/BANNIERE2.jpg")}
-              alt="banniere"
-              className="mybanniere text-center"
+            <ModalImage
+              small={require("../components/img/smallbanniere.jpg")}
+              large={require("../components/img/largebanniere.jpg")}
+              alt="Mairie d'Aboisso"
+              className="mybanniere"
             />
           </div>
           <div className="col-md-7 text-center">
             <h3 className="h3-responsive textbaniere">
-              MAIRE DE LA COMMUNE D'ABOISSO
+              BIENVENUE A LA MAIRIE D'ABOISSO
             </h3>
             <h5 className="h5-responsive textbaniere">
               Le vrai changement avec Jeremie Alfred N'Gouan
