60405ddf1f67a96fa24d99e890ea9e7afd99386e
emmadal <emmanueldalougou@gmail.com>
2019-05-14 17:25:11 +0200

    add secretariat general

--- src/components/containers/servicesmunicipaux.js
+++ src/components/containers/servicesmunicipaux.js
@@ -1,27 +1,36 @@
 export const servicesmunicipaux = [
   {
     id: "1",
-    title: "Les services Administratifs",
+    title: "Le Secretariat General",
+    info: `Le Secrétaire Général est le premier responsable de l’administration sous l’autorité du Maire, il chapeaute et coordonne l’ensemble des actions communales et dispose de prérogatives particulièrement étendues et variées. Il chapeaute par ailleurs les activités du cabinet du Maire. 
+    Collaborateur direct du Maire et de ses adjoints, il prépare et met en œuvre les décisions du Conseil municipal. Il est le chef de l’administration, il assure la coordination de l’action de l’ensemble des services.
+    Le Secrétaire général de Mairie détient des compétences élargies lui permettant d’encadrer, de suivre la gestion administrative et budgétaire de la commune et de conduire des projets ; 
+    
+    Parmi les attributions du Secrétariat général figurent au tout premier plan la préparation et le bon déroulement des séances du Conseil Municipal. Le rôle du Secrétariat général est fondamental. A l’issue des séances, la mission du Secrétariat général est de faire appliquer les décisions du Conseil municipal par l’ensemble des services municipaux. Il prend également connaissance de tous les courriers « départ et arrivée », tient les registres de délibération du Conseil municipal, les registres des procès-verbaux des Conseils municipaux et des procès-verbaux des commissions. Il est également chargé de la rédaction des arrêtés et des décisions municipales. Il coordonne les activités des services municipaux en vue de leur bonne exécution. Il assure le Secrétariat de séance des réunions des organes (Conseil municipal, Municipalité...) la transmission des documents à la Tutelle (DGDDL). Il préside les réunions de chefs de services.`
+  },
+  {
+    id: "2",
+    title: "Les Services Administratifs",
     info: `Sous l’administration du Secrétariat  général,  les  services administratifs assurent la gestion Administrative du Personnel de la Mairie et du Développement des Ressources Humaines, suivent toutes les questions relatives  au  recensement de la population. Le Chef des Services  Administratifs dirige, supervise et contrôle les services Administratifs et veille à l’application des mesures réglementaires. Il assure le Conseil Juridique et suit les  Affaires Judicaires en liaison avec les Services compétents. Les Services Administratifs sont chargés de l’Etat Civil et des questions relatives au recensement des populations; de l’archivage général des documents communaux; des Affaires Administratives Générales(légalisation et certification); du recensement Militaire; de la gestion  du  personnel de la Mairie. Le service Administratif est dirigé par un Chef qui a sous son contrôle plusieurs bureaux : 
     
     Le Bureau du Service du Personnel - Le Bureau de l’Etat Civil avec les Transcripteurs, les Archives et le Pool Informatique - Le Bureau de la Légalisation, Certification et Bureau Militaire - Le Bureau mariage`
   },
   {
-    id: "2",
-    title: "Les services Socio-culturels",
+    id: "3",
+    title: "Les Services Socio-culturels",
     info: `Les Services Socioculturels constituent un service  transversal. Il prend en compte les actions et activités sociales, culturelles. Ce service a pour missions de : établir et proposer le programme annuel des actions et activités sociales, culturelles et des loisirs - suivre les questions relatives à l’éducation préscolaire, primaire - assurer l’organisation matérielle des fêtes populaires, des tournois sportifs et des loisirs en général;-encadrer les associations de jeunes et de femmes - instruire les dossiers de demandes d’aide et de subventions - suivre les questions relatives à l’insertion professionnelle des jeunes et des femmes - chargé des relations avec les services extérieurs chargés des questions sociales, culturelles et sportives.
 
     `
   },
   {
-    id: "3",
-    title: "Les services Financiers",
+    id: "4",
+    title: "Les Services Financiers",
     info:
       "Les Services Financiers sont compétents en matière de : Mobilisation et de gestion des ressources financières - La budgétisation des actions et opérations de développement.\n Ces services sont animés par un Chef de Service qui est chargé : des bureaux placés sous sa responsabilité - de la définition des grands axes et objet de son service - de l’interface avec la recette municipale - de la production du compte administratif et du budget primitif et modificatif de la commune."
   },
   {
-    id: "4",
-    title: "Les services Techniques",
+    id: "5",
+    title: "Les Services Techniques",
     info: `Les services Techniques sont placés sous la responsabilité du chef de service technique dont les attributions sont les suivantes : organiser au plan structurel et fonctionnel le services techniques - diriger, supervise et contrôle les activités des services techniques - réaliser les études sur la stratégie d’équipement de la commune - participer à la réalisation des dossiers de recherche de financement - participer à l’élaboration du programme triennal - assurer la gestion des plans d’urbanismes, d’aménagement et d’occupation de la commune  - contribuer à la mobilisation des recettes communales - assurer la protection et le suivi des cahiers de charge des acquisitions, prestations techniques et travaux - proposer les services publics urbains à privatiser - assurer l’application et le suivi du volet technique des conventions et contrat de location, de gestion, d’affermage, de concession des services publics - assurer l’application des règlements de police municipale relatifs à la divagation des animaux, au domaine, aux constructions et urbanisme, à la circulation urbaine, au transport, inhumation et exhumation des corps ainsi qu’aux cimetières.`
   }
 ];
